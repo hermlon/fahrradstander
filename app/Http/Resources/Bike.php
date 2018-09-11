1373d980886d2e74e676d4c62888135e7f3cab1f
hermlon <dev.hermlon@yamx.net>
2018-09-11 19:04:16 +0200

    Replaced Fractal with Resources

---
+++ app/Http/Resources/Bike.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Bike extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+          'latitude' => $this->latitude,
+          'longitude' => $this->longitude,
+          'notes' => $this->notes
+        ];
+    }
+}
