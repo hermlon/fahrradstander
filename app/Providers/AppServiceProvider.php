d35f752b227a4331972c20743123467269963957
hermlon <dev.hermlon@yamx.net>
2019-02-02 18:05:13 +0100

    activated data wrapping of json respones again

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Resource::withoutWrapping();
+        // Resource::withoutWrapping();
         Bike::observe(BikeCreating::class);
     }
 
